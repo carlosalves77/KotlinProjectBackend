cf7ec9bc7a7543be5c3f3dff81e1e976c92cd7e2
Carlos Alves <carlosaalves07@outlook.com>
2025-06-18 14:17:53 -0300

    feat: initial commit

---
+++ src/main/kotlin/carl/series/training/springboot/NewProjectsStudy/NewProjectsStudyApplication.kt
@@ -0,0 +1,11 @@
+package carl.series.training.springboot.NewProjectsStudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewProjectsStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<NewProjectsStudyApplication>(*args)
+}
---
+++ src/test/kotlin/carl/series/training/springboot/NewProjectsStudy/NewProjectsStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package carl.series.training.springboot.NewProjectsStudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewProjectsStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
