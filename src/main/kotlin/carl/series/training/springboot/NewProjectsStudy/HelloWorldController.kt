a74410dca2efa2bab6dee2f20559ce72dd6e6e1b
Carlos Alves <carlosaalves07@outlook.com>
2025-06-18 17:25:38 -0300

    feat create  models

---
+++ src/main/kotlin/carl/series/training/springboot/NewProjectsStudy/HelloWorldController.kt
@@ -0,0 +1,14 @@
+package carl.series.training.springboot.NewProjectsStudy
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun helloWorld(): String = "This is a Hello World in endPoint"
+
+}
---
+++ src/main/kotlin/carl/series/training/springboot/NewProjectsStudy/model/Bank.kt
@@ -0,0 +1,7 @@
+package carl.series.training.springboot.NewProjectsStudy.model
+
+data class Bank(
+    val accountNumber: String,
+    val trust: Double,
+    val transactionFee: Int
+)
